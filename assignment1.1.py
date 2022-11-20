a4351a42cfb6a4d7c04b891eb408f2d033ede9e2
Purnima Rana <punamranamagar637@gmail.com>
2022-11-20 19:17:10 +0545

    complete

--- assignment1.1.py
+++ assignment1.1.py
@@ -96,6 +96,17 @@
     # 20000 to 50000               25%
     # 50000 to 100000              30%
     # Above 100000                 35%
+# salary=int(input("Enter the salary: "))
+# if salary<20000:
+#     print(f"Net salary; Rs.{salary-(salary*0.15)}")
+# elif salary>=20000 and salary<50000:
+#     print(f"Net salary; Rs.{salary-(salary*0.25)}")
+# elif salary>=50000 and salary<100000:
+#     print(f"Net salary; Rs.{salary-(salary*0.30)}")
+# elif salary>=100000:
+#     print(f"Net salary; {salary-(salary*0.35)}")
+# else:
+#     print("Error")
 
 
 #12: Given three integers, print the smallest one. (Three integers should be user input)
@@ -104,10 +115,12 @@
 # z=int(input("Enter 3rd number: "))
 # if x<y and x<z:
 #     print('Smallest integer is x')
-# if y<x and y<z:
+# elif y<x and y<z:
 #     print('Smallest integer is y')
-# if z<x and z<y:
+# elif z<x and z<y:
 #     print('Smallest integer is z')
+# else:
+#     print("Error")
 
 
 #13: N students take K apples and distribute them among each other evenly. The remaining (the undivisible) part remains in the basket. How many apples will each 
@@ -116,6 +129,10 @@
 
 #14: Check whether 5 is in list of first 5 natural numbers or not. Hint: List => [1,2,3,4,5]
 # list=[1,2,3,4,5]
+# if 5 in list:
+#     print("It is in the list")
+# else:
+#     print("It is not in the list")
 
 
 #15: Write a program that asks the user for a number in the range of 1 to 12. The program should display the corresponding month, where 
@@ -156,3 +173,241 @@
 # print(x)
 
 
+#17: A school has following rules for grading system:
+# a. Below 25 - F
+# b. 25 to 45 - E
+# c. 45 to 50 - D
+# d. 50 to 60 - C
+# e. 60 to 80 - B
+# f. Above 80 - A
+# Ask user to enter marks and print the corresponding grade.
+# marks=int(input("Enter your total marks: "))
+# per=marks/4
+# print(f"percentage; {per}")
+# if per<25:
+#     print("F")
+# elif per>=25 and per<45:
+#     print("E")
+# elif per>=45 and per<50:
+#     print("D")
+# elif per>=50 and per<60:
+#     print("C")
+# elif per>=60 and per<80:
+#     print("B")
+# elif per>80:
+#     print("A")
+# else:
+#     print("Error")
+
+
+#18: Take input of age of 3 people by user and determine oldest and youngest among them.
+# user1=int(input("Enter your age(user1): "))
+# user2=int(input("Enter your age(user2): "))
+# user3=int(input("Enter your age(user3): "))
+# if user1<user2 and user1<user3:
+#     print("User1 is youngest")
+#     if user1>user2 and user1>3:
+#         print("User1 is oldest")
+# elif user2<user1 and user2<user3:
+#     print("User2 is youngest")
+#     if user2>user1 and user2>user3:
+#         print("User2 is oldest")
+# elif user3<user1 and user3<user2:
+#     print("User3 is yougest")
+#     if user3>user1 and user3>user2:
+#         print("User3 is oldest")
+# else:
+#     print("Error")
+
+
+#19: Write a program to check whether a person is eligible for voting or not. (accept age from user)
+# age=int(input("Enter your age: "))
+# if age=<18:
+#     print("You are not eligible to vote")
+# else:
+#     print("You are eligible to vote")
+
+
+#20: Write a program to check whether a number is divisible by 7 or not.
+# num=int(input("Enter a number: "))
+# if (num % 7)== 0:
+#     print("It is divisible by 7")
+# else:
+#     print("It is not divisible by 7")
+
+
+#21: Accept any city from the user and display monument of that city.
+#   City                                 Monument
+#   Delhi                               Red Fort
+#   Agra                                Taj Mahal
+#   Jaipur                              Jal Mahal
+# city=input("Enter name of city: ")
+# if city=="Delhi":
+#     print("Fort")
+# elif city=="Agra":
+#     print("Taj")
+# elif city=="Jaipur":
+#     print("Jal Mahal")
+# else:
+#     print("Error")
+
+
+#22: Write a program to whether a number (accepted from user) is divisible by 2 and 3 both.
+# num=int(input("Enter a number: "))
+# if (num%2)==0 and (num%3)==0:
+#     print("It is divisible by 2 and 3")
+# else: 
+#     print("It is not divisible by 2 and 3")
+
+
+#23: Write a program to accept two numbers and mathematical operators and perform operation accordingly.
+# Like:
+# Enter First Number: 7
+# Enter Second Number : 9
+# Enter operator : +
+# Your Answer is : 16
+# num1=int(input("Enter 1st number: "))
+# num2=int(input("Enter 2nd number: "))
+# operator=input("Enter operator[+,-,*,%]: ")
+# if operator=="+":
+#     print(f"Addition; {num1+num2}")
+# elif operator=="-":
+#     print(f"Subtraction; {num1-num2}")
+# elif operator=="*":
+#     print(f"Multiplication; {num1*num2}")
+# elif operator=="%":
+#     print(f"Division; {num1/num2}")
+# else:
+#     print("Error")
+
+
+#24: Write the syntax of simple if statement.
+
+
+#25: Write a program to display "Hello" if a number entered by user is a multiple of five, otherwise print "Bye".
+# num=int(input("Enter a number: "))
+# if (num%5)==0:
+#     print("Hello")
+# else:
+#     print("Invalid")
+
+
+#26: Write a program to accept a number from 1 to 7 and display the name of the day like 1 for sunday, 2 for monday and so on.
+# num=int(input("Enter a number: "))
+# if num==1:
+#     print("Sunday")
+# elif num==2:
+#     print("Monday")
+# elif num==4:
+#     print("Tuesday")
+# elif num==5:
+#     print("Wednesday")
+# elif num==6:
+#     print("Thursday")
+# elif num==7:
+#     print("Friday")
+# else:
+#     print("Error")
+
+
+#27: Write the logical expression for the following:
+# A is greater than B and C is greater than D
+
+
+
+#28: Write a program to check whether a number entered is three digit number or not.
+# num=int(input("Enter a number: "))
+# if num>=100 and num<1000:
+#     print("It is a three-digit number")
+# else:
+#     print("It is not three-digit number")
+
+
+#29: Write a program to check whether a person is senior citizen or not.
+# age=int(input("Enter your age: "))
+# if age>=
+
+
+#30: Write a program to find the lowest number out of two numbers expected from user.
+# num1=int(input("Enter 1st number: "))
+# num2=int(input("Enter 2nd number: "))
+# if num1<num2:
+#     print("1st number is the lowest number")
+# elif num1>num2:
+#     print("2nd number is the lowest number" )
+# else:
+#     print("Error")
+
+
+#31: Out of "elif" and "else if",  which is the correct statement in python?
+# "Elif" is the correct statement in python.
+
+
+#32: Accept the following from the user and calculate the percentage of class attended:
+# a. Total number of working days
+# b. Total number of days for absent
+# After calculating percentage show that, if the percentage is less than 75, than student will not be able to sit in exam.
+# w_days=int(input("Enter the number of working days: "))
+# a_days=int(input("Enter the number of absent days: "))
+# print(f"Working days; {}")
+
+
+#33:  Write a program to accept percentage and display the category according to the following criteria:
+# Percentage                    Category
+# <40		                      Failed
+# >=40 and <55                    Fair
+# >=55 and <65	                  Good
+# >=65		                     Excellent	
+
+
+#34:  Accept the age, sex('M', 'F'), number of days and display the wages accordingly.
+# Age		     Sex     	   Wage/day
+# >=18 and <30    M	             700
+# 		          F	             750
+# >=30 and <=40	  M	             800
+# 		          F	             850
+# age=int(input("Enter your age: "))
+# sex=input("Enter your sex[M,F]: ")
+# work_days=int(input("Enter your wrok days: "))
+# if age>=18 and age<30 and sex=="M":
+#     print
+
+
+#35: Accept three numbers from the user and display the second largest number.
+
+
+#36: Accept the number of days from the user and calculaye the charge for library according to following:
+# Till five days: Rs 2/day
+# Six to ten days: Rs 3/day
+# 11 to 15 days: Rs 4/day
+# After 15 days: Rs 5/day
+# days=int(input("Enter the number of days: "))
+# if days<=5:
+#     print(f"Amount; Rs.{2*days}")
+# elif days>=6 and days<=10:
+#     print(f"Amount; Rs.{3*days}")
+# elif days>=11 and days<=15:
+#     print(f"Amount; Rs.{4*days}")
+# elif days<=15:
+#     print(f"Amount; Rs.{5*days}")
+# else:
+#     print("Cannot be kept for too many days") 
+
+
+#37: Evaluate the following statements:
+# a=True
+# b=True
+# c=True
+# d=True
+# print(c)
+# print(d)
+# print(not a)
+# print(not b)
+# print(not c)
+# print(not d)
+# print(a and b)
+# print(a or b)
+# print(a and b or c)
+# print(not a or b or c)
+# print(not a or not b or not c)
+# print(not(not a or not b or not c))
