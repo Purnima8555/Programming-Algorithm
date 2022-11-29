5d9002cba02ac9bbdb497de495cc0644eda417e9
Purnima Rana <punamranamagar637@gmail.com>
2022-11-29 23:38:58 +0545

    completed

---
+++ assignment1.4.py
@@ -0,0 +1,103 @@
+# Pattern Design:
+#Writing the letters of my name:
+
+# for row in range(6):
+#     for column in range(5):
+#         if((column==0) or (column==1) and (row!=1 and row!=2 and row!=4 and row!=5 and row!=6) or (column==2) and (row!=1 and row!=2 and row!=4 and row!=5 and row!=6) or (column==3) and (row!=1 and row!=2 and row!=4 and row!=5 and row!=6) or (column==4) and (row!=0 and row!=3 and row!=4 and row!=5 and row!=6)):
+#             print('*',end=' ')
+#         else:
+#             print(end='  ')
+#     print()
+
+
+# for row in range(6):
+#     for column in range(5):
+#         if ((column==0) and (row!=5) or (column==1) and (row!=0 and row!=1 and row!=2 and row!=3 and row!=4) or (column==2) and (row!=0 and row!=1 and row!=2 and row!=3 and row!=4) or (column==3) and (row!=0 and row!=1 and row!=2 and row!=3 and row!=4) or (column==4) and (row!=5)):
+#             print('*',end=' ')
+#         else:
+#             print(end='  ')
+#     print()
+
+
+# for row in range(6):
+#     for column in range(5):
+#         if ((column==0) or (column==1) and (row!=1 and row!=2 and row!=4 and row!=5 and row!=6) or (column==2) and (row!=1 and row!=2 and row!=5 and row!=6) or (column==3) and (row!=1 and row!=2 and row!=4 and row!=6) or (column==4) and (row!=0 and row!=3 and row!=4 and row!=5)):
+#             print('*',end=' ')
+#         else:
+#             print(end='  ')
+#     print()
+
+
+# for row in range(6):
+#     for column in range(6):
+#         if ((column==0) or (column==1) and (row!=0 and row!=2 and row!=3 and row!=4 and row!=5) or (column==2) and (row!=0 and row!=1 and row!=3 and row!=4 and row!=5) or (column==3) and (row!=0 and row!=1 and row!=2 and row!=4 and row!=5) or (column==4) and (row!=0 and row!=1 and row!=2 and row!=3 and row!=5) or (column==5)):
+#             print('*',end=' ')
+#         else:
+#             print(end='  ')
+#     print()
+
+
+# for row in range(7):
+#     for column in range(6):
+#         if ((column==0) and (row!=1 and row!=2 and row!=3 and row!=4 and row!=5) or (column==1) and (row!=1 and row!=2 and row!=3 and row!=4 and row!=5) or (column==2) or (column==3) and (row!=1 and row!=2 and row!=3 and row!=4 and row!=5) or (column==4) and (row!=1 and row!=2 and row!=3 and row!=4 and row!=5)):
+#             print('*',end=' ')
+#         else:
+#             print(end='  ')
+#     print()
+
+
+# for row in range(6):
+#     for column in range(5):
+#         if ((column==0) or (column==1) and (row!=0 and row!=2 and row!=3 and row!=3 and row!=4 and row!=5) or (column==2) and (row!=0 and row!=1 and row!=3 and row!=3 and row!=4 and row!=5) or (column==3) and (row!=0 and row!=2 and row!=3 and row!=3 and row!=4 and row!=5) or (column==4)):
+#             print('*',end=' ')
+#         else:
+#             print(end='  ')
+#     print()
+
+
+# for row in range(6):
+#     for column in range(5):
+#         if((column==0) and (row!=0) or (column==1) and (row==0 or row==3) or (column==2) and (row==0 or row==3) or (column==3) and (row==0 or row==3) or (column==4) and (row!=0)):
+#             print('*',end=' ')
+#         else:
+#             print(end='  ')
+#     print()
+
+
+# for row in range(6):
+#     for column in range(5):
+#         if ((column==0) or (column==1) and (row!=1 and row!=2 and row!=4 and row!=5 and row!=6) or (column==2) and (row!=1 and row!=2 and row!=5 and row!=6) or (column==3) and (row!=1 and row!=2 and row!=4 and row!=6) or (column==4) and (row!=0 and row!=3 and row!=4 and row!=5)):
+#             print('*',end=' ')
+#         else:
+#             print(end='  ')
+#     print()
+
+
+# for row in range(6):
+#     for column in range(5):
+#         if((column==0) and (row!=0) or (column==1) and (row==0 or row==3) or (column==2) and (row==0 or row==3) or (column==3) and (row==0 or row==3) or (column==4) and (row!=0)):
+#             print('*',end=' ')
+#         else:
+#             print(end='  ')
+#     print()
+
+
+# for row in range(6):
+#     for column in range(6):
+#         if ((column==0) or (column==1) and (row!=0 and row!=2 and row!=3 and row!=4 and row!=5) or (column==2) and (row!=0 and row!=1 and row!=3 and row!=4 and row!=5) or (column==3) and (row!=0 and row!=1 and row!=2 and row!=4 and row!=5) or (column==4) and (row!=0 and row!=1 and row!=2 and row!=3 and row!=5) or (column==5)):
+#             print('*',end=' ')
+#         else:
+#             print(end='  ')
+#     print()
+
+
+# for row in range(6):
+#     for column in range(5):
+#         if((column==0) and (row!=0) or (column==1) and (row==0 or row==3) or (column==2) and (row==0 or row==3) or (column==3) and (row==0 or row==3) or (column==4) and (row!=0)):
+#             print('*',end=' ')
+#         else:
+#             print(end='  ')
+#     print()
+
+
+# Writing name in patterns in one ROW:
