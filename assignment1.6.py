7db4650436a74de9242871180a6d6530eabb681a
Purnima Rana <punamranamagar637@gmail.com>
2022-12-30 22:21:15 +0545

    Function

--- assignment1.6.py
+++ assignment1.6.py
@@ -1,3 +1,5 @@
+# FUNCTION QUESTION AND ANSWERS:
+
 #1. Write a program for students result processing for school. You have marks obtained by student in 5 subjects.
 # english=int(input("Enter your marks: "))
 # nepali=int(input("Enter your marks: "))
