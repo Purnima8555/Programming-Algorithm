9a1b3325e6c9cd874f7d073059abd6aef93137f6
Purnima Rana <punamranamagar637@gmail.com>
2022-12-30 22:01:47 +0545

    While Loop

--- assignment1.5.py
+++ assignment1.5.py
@@ -5,25 +5,93 @@
 
 
 #2. Sum of list:
+
+
+
 #3. Print each character using indexing:
+
+
+
 #4. Program to display each element of a list:
+
+
+
 #5. Multuplication of each element:
+
+
+
 #6. Multiplication table of a given number:
+
+
+
 #7. Reverse a list:
+
+
+
 #8. Program to count the total number of digits in a number:
+
+
+
 #9. Given number is prime or not:
+
+
+
 #10. Python program to count the number of even and odd numbers from a series of numbers:
+
+
+
 #11. Python program that accepts a string and calculatethe numberof digits and letters:
+
+
+
 #12. Python program to check the validity of username and password input by users:
+
+
+
 #13. Program to print the given number is odd or even:
+
+
+
 #14. Factorial of a given number:
+
+
+
 #15. Program to check given number is palindrome or not:
+
+
+
 #16. Program to check given number is armstrong or not:
+
+
+
 #17. Python program to check a number is perfect square:
+
+
+
 #18. Python program to check a number is perfect number:
+
+
+
 #19. Print multiplication table of 1,2,3,4,5,6,7,8:
+
+
+
 #20. Print the first 10 natural numbers using loop:
+
+
+
 #21. Python program to calculate the sum of all the odd numbers within the given range:
+
+
+
 #22. Python program to calculate the sum of all the even numbers within the given range:
+
+
+
 #23. Python program to count the space of a given string:
+
+
+
 #24. Pyhton program that accepts a string and calculatethe number of digits, letters and space: 
+
+
