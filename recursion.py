1f39b2b552507838f6ee21383559aa0f78ce8958
Purnima Rana <punamranamagar637@gmail.com>
2023-01-25 14:30:19 +0545

    notes

--- recursion.py
+++ recursion.py
@@ -9,6 +9,7 @@
 #     y=z
 #     z=x+y
 
+
 # 2. Print 10 number of finonacci series using for loop:
 # n=int(input("Enter number: "))
 # i=int(n)
@@ -23,5 +24,45 @@
 
 
 # 3. Fibonacci series using recursion:
+# def a(n):
+#     if n==1:
+#         return 0
+#     if n==2:
+#         return 1
+#     return a(n-1) + a(n-2)
+# n=int(input("Enter a number: "))
+# for i in range(1, n+1):
+#     print(a(i))
+
+
 # 4. Factorial of a number using recursion:
+# def fact(num):
+#     """ This function call itself to find the factorial of a number """
+#     if num==1:
+#         return 1
+#     else:
+#         return(num*fact(num-1))
+# num=int(input("Enter number: "))
+# print("Factorial of",num,"is:", fact(num))
+
+
 # 5. Reverse string using recursion:
+
+
+# 6. Using recursion get the sum of natural number 10:
+# def fact(num):
+#     if num==1:
+#         return 1
+#     else:
+#         return(num+fact(num-1))
+# num=10
+# print("Sum of", num, "natural number is:", fact(num))
+
+
+# 7. To know the recursion limit and create a certain limit:
+# import sys
+# print(sys.getrecursionlimit())
+# sys.setrecursionlimit(4000)
+# print(sys.getrecursionlimit())
+
+
